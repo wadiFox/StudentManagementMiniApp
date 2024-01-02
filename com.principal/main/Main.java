c652fe67cd45a17e80f90a461bd39796eaf5cb14
Ouadie <elamraniouadie@gmail.com>
2024-01-02 17:31:56 +0100

    StudentDaoInterface implementation, class Main started implemeting

--- com.principal/main/Main.java
+++ com.principal/main/Main.java
@@ -1,10 +1,42 @@
 package main;
 
+import java.util.Scanner;
+
+import model.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to Student Management System");
+		System.out.println();
+		Scanner input = new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
+		while(true) {	
+    		System.out.println("Please choose your action : ");
+    		System.out.println("####---------####");
+    		System.out.println("1 - View Students List\n2 - Add New Student\n3 - Update Student\n4 - Search Student By Registration Number\n5 - Delete Student\n6 - Exit");
+    		int choice=input.nextInt();
+    	switch(choice) {
+        case 1 :
+        	
+        	break;
+        case 2 :
+        	
+        	break;
+        case 3 :
+        	
+        	break;
+        case 4 :
+        	
+        	break;
+        case 5 :
+        	
+        	break;
+        case 6 :
+        	System.out.println("Thanks for using Student Management System\nGood Bye!!!");
+        	System.exit(0);
+        }
+}
 	}
 
 }
--- com.principal/model/StudentDaoInterface.java
+++ com.principal/model/StudentDaoInterface.java
@@ -1,5 +1,9 @@
 package model;
 
-public class StudentDaoInterface {
-
+public interface StudentDaoInterface {
+boolean viewStudentList();	
+boolean addStudent(StudentDTO st);
+boolean updateStudent(int regNumber, String updated,int ch, StudentDTO st);
+boolean searchStudentByRegNumber(int regNumber);
+boolean deletStudent(int regNumber);
 }
